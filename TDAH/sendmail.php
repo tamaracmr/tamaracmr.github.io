e89b02d695e79eeaf41f1b2d07170a7d54f6cd54
Tamara Campomar <tamaracampomarr@gmail.com>
2020-06-30 08:31:14 +0200

    deleter tdah

--- TDAH/sendmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Settings
-$settings = array(
-    "name"          => "Maria Fernanda",
-    "email"         => "tdahaxarquia@gmail.com",
-);
-
-require_once "phpmailer/contact_form.php";
